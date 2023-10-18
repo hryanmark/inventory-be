2a3c5d63e3fba1c508a645fbf2fb7ebcefdb264a
Ryan Mark Hiluano <hryanmark@gmail.com>
2023-10-18 17:08:35 +0900

    minor changes in datatypes

--- src/main/java/com/example/inventorybe/model/ItemEntity.java
+++ src/main/java/com/example/inventorybe/model/ItemEntity.java
@@ -1,5 +1,8 @@
 package com.example.inventorybe.model;
 
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,9 +17,9 @@ public class ItemEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
-	private Integer brand_id;
+	private BigInteger brand_id;
 	
-	private Integer category_id;
+	private BigInteger category_id;
 	
 	private String title;
 	
@@ -24,13 +27,13 @@ public class ItemEntity {
 	
 	private String SKU;
 	
-	private String base_uom;
+	private BigInteger base_uom;
 	
-	private String sales_uom;
+	private BigInteger sales_uom;
 	
-	private String puchase_uom;
+	private BigInteger puchase_uom;
 	
-	private Integer unit_cost;
+	private Double unit_cost;
 	
 	private Integer minimum_stock_level;
 	
@@ -40,43 +43,20 @@ public class ItemEntity {
 	
 	private String created_by;
 	
-	private String created_at;
-	
 	private String updated_by;
 	
-	private String updated_at;
+	private LocalDateTime created_at;
+	
+	private LocalDateTime updated_at;
 
 	public ItemEntity() {
 		super();
 	}
 
-	public ItemEntity(Integer brand_id, Integer category_id, String title, String description, String sKU,
-			String base_uom, String sales_uom, String puchase_uom, Integer unit_cost, Integer minimum_stock_level,
-			Integer maximum_stock_level, Integer status, String created_by, String created_at, String updated_by,
-			String updated_at) {
-		super();
-		this.brand_id = brand_id;
-		this.category_id = category_id;
-		this.title = title;
-		this.description = description;
-		SKU = sKU;
-		this.base_uom = base_uom;
-		this.sales_uom = sales_uom;
-		this.puchase_uom = puchase_uom;
-		this.unit_cost = unit_cost;
-		this.minimum_stock_level = minimum_stock_level;
-		this.maximum_stock_level = maximum_stock_level;
-		this.status = status;
-		this.created_by = created_by;
-		this.created_at = created_at;
-		this.updated_by = updated_by;
-		this.updated_at = updated_at;
-	}
-
-	public ItemEntity(Long id, Integer brand_id, Integer category_id, String title, String description, String sKU,
-			String base_uom, String sales_uom, String puchase_uom, Integer unit_cost, Integer minimum_stock_level,
-			Integer maximum_stock_level, Integer status, String created_by, String created_at, String updated_by,
-			String updated_at) {
+	public ItemEntity(Long id, BigInteger brand_id, BigInteger category_id, String title, String description,
+			String sKU, BigInteger base_uom, BigInteger sales_uom, BigInteger puchase_uom, Double unit_cost,
+			Integer minimum_stock_level, Integer maximum_stock_level, Integer status, String created_by,
+			String updated_by, LocalDateTime created_at, LocalDateTime updated_at) {
 		super();
 		this.id = id;
 		this.brand_id = brand_id;
@@ -92,8 +72,8 @@ public class ItemEntity {
 		this.maximum_stock_level = maximum_stock_level;
 		this.status = status;
 		this.created_by = created_by;
-		this.created_at = created_at;
 		this.updated_by = updated_by;
+		this.created_at = created_at;
 		this.updated_at = updated_at;
 	}
 
@@ -105,19 +85,19 @@ public class ItemEntity {
 		this.id = id;
 	}
 
-	public Integer getBrand_id() {
+	public BigInteger getBrand_id() {
 		return brand_id;
 	}
 
-	public void setBrand_id(Integer brand_id) {
+	public void setBrand_id(BigInteger brand_id) {
 		this.brand_id = brand_id;
 	}
 
-	public Integer getCategory_id() {
+	public BigInteger getCategory_id() {
 		return category_id;
 	}
 
-	public void setCategory_id(Integer category_id) {
+	public void setCategory_id(BigInteger category_id) {
 		this.category_id = category_id;
 	}
 
@@ -145,35 +125,35 @@ public class ItemEntity {
 		SKU = sKU;
 	}
 
-	public String getBase_uom() {
+	public BigInteger getBase_uom() {
 		return base_uom;
 	}
 
-	public void setBase_uom(String base_uom) {
+	public void setBase_uom(BigInteger base_uom) {
 		this.base_uom = base_uom;
 	}
 
-	public String getSales_uom() {
+	public BigInteger getSales_uom() {
 		return sales_uom;
 	}
 
-	public void setSales_uom(String sales_uom) {
+	public void setSales_uom(BigInteger sales_uom) {
 		this.sales_uom = sales_uom;
 	}
 
-	public String getPuchase_uom() {
+	public BigInteger getPuchase_uom() {
 		return puchase_uom;
 	}
 
-	public void setPuchase_uom(String puchase_uom) {
+	public void setPuchase_uom(BigInteger puchase_uom) {
 		this.puchase_uom = puchase_uom;
 	}
 
-	public Integer getUnit_cost() {
+	public Double getUnit_cost() {
 		return unit_cost;
 	}
 
-	public void setUnit_cost(Integer unit_cost) {
+	public void setUnit_cost(Double unit_cost) {
 		this.unit_cost = unit_cost;
 	}
 
@@ -209,14 +189,6 @@ public class ItemEntity {
 		this.created_by = created_by;
 	}
 
-	public String getCreated_at() {
-		return created_at;
-	}
-
-	public void setCreated_at(String created_at) {
-		this.created_at = created_at;
-	}
-
 	public String getUpdated_by() {
 		return updated_by;
 	}
@@ -225,11 +197,19 @@ public class ItemEntity {
 		this.updated_by = updated_by;
 	}
 
-	public String getUpdated_at() {
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
+
+	public LocalDateTime getUpdated_at() {
 		return updated_at;
 	}
 
-	public void setUpdated_at(String updated_at) {
+	public void setUpdated_at(LocalDateTime updated_at) {
 		this.updated_at = updated_at;
 	}
 
@@ -239,7 +219,7 @@ public class ItemEntity {
 				+ ", description=" + description + ", SKU=" + SKU + ", base_uom=" + base_uom + ", sales_uom="
 				+ sales_uom + ", puchase_uom=" + puchase_uom + ", unit_cost=" + unit_cost + ", minimum_stock_level="
 				+ minimum_stock_level + ", maximum_stock_level=" + maximum_stock_level + ", status=" + status
-				+ ", created_by=" + created_by + ", created_at=" + created_at + ", updated_by=" + updated_by
+				+ ", created_by=" + created_by + ", updated_by=" + updated_by + ", created_at=" + created_at
 				+ ", updated_at=" + updated_at + "]";
 	}
 	
