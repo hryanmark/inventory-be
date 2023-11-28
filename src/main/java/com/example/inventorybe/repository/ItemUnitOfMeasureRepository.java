b5b746f2abfe8ae14dd91a81665658aa76118da5
Ryan Mark Hiluano <hryanmark@gmail.com>
2023-11-28 16:14:06 +0900

    removed unused packages

--- src/main/java/com/example/inventorybe/repository/ItemUnitOfMeasureRepository.java
+++ src/main/java/com/example/inventorybe/repository/ItemUnitOfMeasureRepository.java
@@ -3,8 +3,6 @@ package com.example.inventorybe.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.example.inventorybe.model.ItemUnitOfMeasureEntity;
